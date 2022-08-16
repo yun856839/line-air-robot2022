f1e02a4ab6735065fe60762ac48e5c107c31b25b
yun <n856839@gmail.com>
2022-08-16 12:41:23 +0800

    Add AQI crawler feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const linebot = require('linebot')
+const axios = require('axios')
 
 const PORT = process.env.PORT || 3000
 
@@ -8,25 +9,28 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
+const apiKEY = process.env.API_KEY
+const apiURL = `https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=${apiKEY}`
+
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
 	channelSecret: process.env.CHANNEL_SECRET,
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 })
 const linebotParser = bot.parser()
-bot.on('message', function (event) {
-	event
-		.reply(event.message.text)
-		.then(function (data) {
-			console.log('success', data) // success
-		})
-		.catch(function (error) {
-			console.log('success', error) // error
-		})
-})
 
-app.get('/', (req, res) => {
-	res.send('hello----')
+bot.on('message', async function (event) {
+	let msg = ''
+	let air = await axios.get(apiURL)
+
+	console.log(air.data.records)
+
+	for (let i in air.data.records) {
+		if (air.data.records[i].sitename.includes(event.message.text)) {
+			msg += `${air.data.records[i].sitename} , ${air.data.records[i].status} , ${air.data.records[i].pollutant}\n`
+		}
+	}
+	msg ? event.reply(`${msg}`) : event.reply('找不到資料，可能文字輸入錯誤')
 })
 
 app.post('/linewebhook', linebotParser)
